9f352fce6ee3b18271ea39f5d42bea8b9446e3ef
TheAppMaker67 <bigbraingamingcenter1@gmail.com>
2026-07-12 20:17:56 -0400

    Update import paths for screens and context

--- App.js
+++ App.js
@@ -2,10 +2,10 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
-import { UserProvider } from "./src/context/UserContext";
-import HomeScreen from "./src/screens/HomeScreen";
-import CameraConfirmScreen from "./src/screens/CameraConfirmScreen";
-import PaywallScreen from "./src/screens/PaywallScreen";
+import { UserProvider } from "src/context/UserContext.js";
+import HomeScreen from "src/screens/HomeScreen.js";
+import CameraConfirmScreen from "src/screens/CameraConfirmScreen.js";
+import PaywallScreen from "src/screens/PaywallScreen.js";
 
 const Stack = createNativeStackNavigator();
 
